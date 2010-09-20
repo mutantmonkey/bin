08c129654e3430e45bae76c1e673275d62c60d26
mutantmonkey <mutantmonkey@gmail.com>
2010-09-20 18:12:08 -0400

    Added check_greader.py to check Google Reader unread count

---
+++ check_greader.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#################################################################################
+# check_greader.py - Check unread posts in Google Reader
+#
+# author: jimmyorr@stackoverflow
+# author: livibetter@stackoverflow
+# author: mutantmonkey <mutantmonkey@gmail.com>
+################################################################################
+
+import urllib, urllib2
+import json
+import keyring
+
+username = 'mutantmonkey@gmail.com'
+password = keyring.get_password('google.com', username)
+
+def store_password():
+	import getpass
+
+	password = getpass.getpass("Password for %s: " % username)
+	keyring.set_password('google.com', username, password)
+
+if not password:
+	store_password()
+
+# Authenticate to obtain Auth
+auth_url = 'https://www.google.com/accounts/ClientLogin'
+auth_req_data = urllib.urlencode({'Email': username,
+                                  'Passwd': password,
+                                  'service': 'reader'})
+auth_req = urllib2.Request(auth_url, data=auth_req_data)
+auth_resp = urllib2.urlopen(auth_req)
+auth_resp_content = auth_resp.read()
+auth_resp_dict = dict(x.split('=') for x in auth_resp_content.split('\n') if x)
+# SID = auth_resp_dict["SID"]
+AUTH = auth_resp_dict["Auth"]
+
+# Create a cookie in the header using the Auth
+header = {}
+header['Authorization'] = 'GoogleLogin auth=%s' % AUTH
+
+reader_base_url = 'http://www.google.com/reader/api/0/unread-count?%s'
+reader_req_data = urllib.urlencode({'all': 'true',
+                                    'output': 'json'})
+reader_url = reader_base_url % (reader_req_data)
+reader_req = urllib2.Request(reader_url, None, header)
+reader_resp = urllib2.urlopen(reader_req)
+
+data = json.loads(reader_resp.read())
+if len(data['unreadcounts']) > 0:
+	print int(data['unreadcounts'][0]['count'])
+else:
+	print 0
+
