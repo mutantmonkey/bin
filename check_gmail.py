2c42899547aa9247480d20f4480f359b6f6169ee
mutantmonkey <mutantmonkey@gmail.com>
2010-09-20 11:19:34 -0400

    Added Gmail check script that uses IMAP to get unread count

---
+++ check_gmail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import keyring
+import imaplib
+import re
+
+server = 'imap.gmail.com'
+port = 993
+
+username = 'mutantmonkey'
+password = keyring.get_password(server, username)
+
+mailbox = 'INBOX'
+
+def store_password():
+	import getpass
+
+	password = getpass.getpass("Password for %s@%s: " % (username, server))
+	keyring.set_password(server, username, password)
+
+if not password:
+	store_password()
+
+# connect to server and login
+conn = imaplib.IMAP4_SSL(server, port)
+conn.login(username, password)
+
+unread = re.compile("UNSEEN (\d+)")
+unread_count = unread.search(conn.status(mailbox, '(UNSEEN)')[1][0]).group(1)
+print unread_count
+
+conn.logout()
+
