6d3484f8d12615aa956fb4e95c6e7c25c755bd1d
mutantmonkey <mutantmonkey@gmail.com>
2011-02-07 22:52:06 -0500

    Split bars from dwm start script

---
+++ start_bars.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+source $XDG_CONFIG_HOME/bars.conf
+
+# Start stalonetray
+stalonetray -c ~/.config/stalonetrayrc -bg "${BAR_BG}" -geometry ${BAR_TRAY_SLOTS}x1+${BAR_TRAY_X}+0 &
+
+# Start dzen2
+conky -c ~/.config/dzen/conkyrc | dzen2 -e 'button3=' -bg "${BAR_BG}" -fg "${BAR_FG}" -fn "${BAR_FONT}" -ta r -x "${BAR_RIGHT_X}" &
+
--- start_dwm.sh
+++ start_dwm.sh
@@ -7,28 +7,11 @@ export OOO_FORCE_DESKTOP=gnome
 xsetroot -cursor_name left_ptr
 
 # Start statnot (should be configured to write to ~/.statnot)
-statnot &
+#statnot &
 
-# Bars {{{
-
-BAR_FONT="DejaVu Sans Mono-9"
-BAR_FG="#dcdccc"
-BAR_BG="#3f3f3f"
-
-BAR_LEFT_WIDTH=300
-BAR_TRAY_X=300
-BAR_TRAY_SLOTS=6
-BAR_RIGHT_X=402
-
-# Start stalonetray
-(sleep 2 && stalonetray -c ~/.config/stalonetrayrc -bg "${BAR_BG}" -geometry ${BAR_TRAY_SLOTS}x1+${BAR_TRAY_X}+0) &
-
-# Start dzen2
-(sleep 2 && conky -c ~/.config/dzen/conkyrc | dzen2 -bg "${BAR_BG}" -fg "${BAR_FG}" -fn "${BAR_FONT}" -ta r -x "${BAR_RIGHT_X}") &
-
+# Start bars
+(sleep 2 && start_bars.sh) &
 #conky -c ~/.config/dwm/conkyrc | while read -r; do xsetroot -name "$REPLY"; done &
-
-# }}}
 
 # Run xdg startup apps
 (sleep 2 && dex -a) &
@@ -42,13 +25,5 @@ update_unread_counts() {
 };
 (sleep 2 && update_unread_counts) &
 
-#exec dwm
-
-# Start dwm in an infinite loop so that we can restart it without killing apps
-while true; do
-	# Log stderror to a file 
-	dwm 2> ~/.dwm.log
-	# No error logging
-	#dwm >/dev/null 2>&1
-done
+exec dwm
 
