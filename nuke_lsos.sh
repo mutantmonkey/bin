03c5f4338a92f0b6373105fa6243fbbd6e6fe284
mutantmonkey <mutantmonkey@gmail.com>
2011-02-07 22:54:48 -0500

    Add nuke LSOs script

---
+++ nuke_lsos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#################################################################################
+# nuke_lsos.sh - Nuke Adobe Flash Player local shared objects
+#
+# author: mutantmonkey <mutantmonkey@gmail.com>
+################################################################################
+
+rm -rf $HOME/{.adobe,.macromedia}
+
