e352cc9d5da73607dd3ee2317c36dcaef3afca9c
mutantmonkey <mutantmonkey@gmail.com>
2011-03-12 20:12:30 -0500

    start_dwm.sh: use SPAWN_DELAY instead of hardcoded delay

--- start_dwm.sh
+++ start_dwm.sh
@@ -6,11 +6,11 @@
 ################################################################################
 
 # Start bars
-(sleep 2 && start_bars.sh) &
+(sleep $SPAWN_DELAY && start_bars.sh) &
 #conky -c ~/.config/dwm/conkyrc | while read -r; do xsetroot -name "$REPLY"; done &
 
 # Run xdg startup apps
-(sleep 2 && dex -a) &
+dex -a
 
 # Hack to get unread counts working
 update_unread_counts() {
@@ -19,7 +19,7 @@ update_unread_counts() {
 	sleep 300
 	update_unread_counts
 };
-(sleep 2 && update_unread_counts) &
+(sleep $SPAWN_DELAY && update_unread_counts) &
 
 exec dwm
 
