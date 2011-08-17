1eacec37da17545b66f04b11f298e65722708bc1
mutantmonkey <mutantmonkey@mutantmonkey.in>
2011-08-16 20:30:30 -0400

    check_mail.py: no notifications for opennic lists

--- check_mail.py
+++ check_mail.py
@@ -42,8 +42,9 @@ for mailbox_list_resp in mailboxes:
 	flags, delim, mailbox = list_response_regex.match(mailbox_list_resp).groups()
 	mailbox = mailbox.strip('"')
 	if mailbox in ('aur', 'bugs', 'cron', 'drafts', 'lists/novalug',
-			'lists/full-disclosure', 'lists/mappingdc',
-			'logwatch', 'shopping/deals', 'sent', 'spam'):
+			'lists/full-disclosure', 'lists/mappingdc', 'lists/opennic',
+			'lists/opennic/dns-operations', 'logwatch', 'shopping/deals',
+			'sent', 'spam'):
 		continue
 	mbox_status = conn.status(mailbox, '(UNSEEN)')[1][0]
 	unread_match = unread_regex.search(mbox_status)
