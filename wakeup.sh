075a72e3cf2ff8603d123764fc94c7e29329497a
mutantmonkey <mutantmonkey@mutantmonkey.in>
2011-10-16 01:12:19 -0400

    remove some old stuff, add wakeup.sh

---
+++ wakeup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+################################################################################
+# wakeup.sh - mpd alarm script using mpc
+#
+# author: mutantmonkey <mutantmonkey@mutantmonkey.in>
+################################################################################
+
+mpc clear
+mpc load Electronic
+mpc play
