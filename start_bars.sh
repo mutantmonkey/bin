7016a9fa9ece38cbf40742c6f59fd7170d790dfe
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-06-23 00:58:58 -0700

    simply start_bars.sh

--- start_bars.sh
+++ start_bars.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 #################################################################################
-# start_bars.sh - bar script script (stalonetray and dzen2)
+# start_bars.sh - bar script script (dzen2)
 #
-# author: mutantmonkey <mutantmonkey@gmail.com>
+# author: mutantmonkey <mutantmonkey@mutantmonkey.in>
 ################################################################################
 
-source $XDG_CONFIG_HOME/bars.conf
-
-# Start stalonetray
-stalonetray -c ~/.config/stalonetrayrc -bg "${BAR_BG}" -geometry ${BAR_TRAY_SLOTS}x1+${BAR_TRAY_X}+0 &
-
-# Start dzen2
-conky -c ~/.config/dzen/conkyrc | dzen2 -e 'button3=' -bg "${BAR_BG}" -fg "${BAR_FG}" -fn "${BAR_FONT}" -ta r -x "${BAR_RIGHT_X}" &
-
+conky -q -c ~/.config/dzen/conkyrc | dzen2 -dock -e 'button3=' -ta r -xs 1 &
