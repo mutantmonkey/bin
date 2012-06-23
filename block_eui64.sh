d64a8e5cc6a21c16742dce1070f9bd84a495093b
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-06-23 03:24:31 -0700

    block_eui64.sh: quit if no IPv6 adddress added

--- block_eui64.sh
+++ block_eui64.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 # block_eui64.sh - block all traffic on EUI-64 generated address
 #
@@ -8,6 +8,8 @@
 IF=$1
 IPADDR=$(ip -6 addr show $IF | grep "scope global dynamic" | sed 's/^.*inet6 \(.*\)\/.*$/\1/')
 IP6T=$(which ip6tables)
+
+[ -z "$IPADDR" ] && exit 0
 
 $IP6T -C INPUT -d $IPADDR -j DROP 2>/dev/null
 [ $? != 0 ] && $IP6T -I INPUT -d $IPADDR -j DROP
