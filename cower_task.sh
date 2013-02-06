a6d16b05b91e409bbc5b3791b987c5b429cd9cd3
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-02-06 14:01:50 -0500

    add cower_task.sh

---
+++ cower_task.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pkgs=$(cower -bu --ignorerepo=mutantmonkey | sed 's/^:: \([A-Za-z0-9\-]*\) .*$/\1/')
+for pkg in $pkgs; do
+    description="Update package: $pkg"
+
+    task "$description" >/dev/null
+    if [ $? -eq 1 ]; then
+        task add "$description" +arch
+    fi
+done
