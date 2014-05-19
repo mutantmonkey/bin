b768f9345077dc51f5412e50b8b13a21375ad614
mutantmonkey <mutantmonkey@mutantmonkey.in>
2014-05-19 12:21:12 -0700

    start_tmux.sh: some tweaks, switch to alot

--- start_tmux.sh
+++ start_tmux.sh
@@ -6,9 +6,16 @@
 ################################################################################
 
 tmux -u new -d -s0 -nirc
-tmux neww -d -t2 -nmutt
+#tmux neww -d -t2 -nmutt
+
+tmux neww -d -t2 -nalot 'exec alot'
+tmux set-window-option -t2 remain-on-exit on
+
 tmux neww -d -t3 -ntask
 tmux neww -d -t4 -nncmpcpp
 
-tmux -u new -d -smutt -t0
+tmux -u new -d -salot -t0
+#tmux set -tmutt status off
+
 tmux -u new -d -sncmpcpp -t0
+#tmux set -tncmpcpp status off
