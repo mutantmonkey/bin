2cda5db81180685e62fd7e5314357dd2dd84b4f0
mutantmonkey <mutantmonkey@mutantmonkey.in>
2014-09-29 21:32:35 -0700

    add openswan to pem utility

---
+++ openswan_to_pem.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import base64
+import binascii
+from Crypto.PublicKey import RSA
+
+comps = {}
+
+with open('/etc/ipsec.secrets') as f:
+    for line in f:
+        line = line.split(':', 1)
+        if len(line) == 2:
+            k = line[0].strip()
+
+            v = line[1].strip()
+            if v.startswith('0s'):
+                v = int(v[2:], 64)
+            elif v.startswith('0x'):
+                v = int(v[2:], 16)
+
+            comps[k] = v
+
+r = RSA.construct((comps['Modulus'], comps['PublicExponent'],
+                   comps['PrivateExponent']))
+print(r.exportKey('PEM').decode('ascii'))
