de106a6172b21f990b32a1f4858dbaa4e2008e33
mutantmonkey <mutantmonkey@mutantmonkey.in>
2014-10-12 20:55:02 -0700

    repo.py: add check updates feature
    adds dependencies on pyalpm and requests

--- repo.py
+++ repo.py
@@ -3,38 +3,38 @@
 import argparse
 import glob
 import os.path
+import pyalpm
+import requests
 import subprocess
+import sys
 import tarfile
 
 
 class Package(object):
-    def __init__(self, name, ver=None, rel=None):
-        if ver is not None and rel is not None:
+    def __init__(self, name, ver=None):
+        if ver is not None:
             self.name = name
             self.ver = ver
-            self.rel = rel
         else:
             self.name = name[0]
             self.ver = name[1]
-            self.rel = name[2]
 
     def __lt__(self, other):
-        # FIXME: also compare version and release
+        # FIXME: also compare version
         return self.name < other.name
 
     def __gt__(self, other):
-        # FIXME: also compare version and release
+        # FIXME: also compare version
         return self.name > other.name
 
     def __eq__(self, other):
         return hash(self) == hash(other)
 
     def __hash__(self):
-        return hash((self.name, self.ver, self.rel))
+        return hash((self.name, self.ver))
 
     def __repr__(self):
-        return "{name} {ver}-{rel}".format(name=self.name, ver=self.ver,
-                                           rel=self.rel)
+        return "{name} {ver}".format(name=self.name, ver=self.ver)
 
 
 def list_packages(repo):
@@ -43,6 +43,7 @@ def list_packages(repo):
         for tarinfo in tar:
             if tarinfo.isdir():
                 comps = tarinfo.name.rsplit('-', 2)
+                comps[1] += '-' + comps[2]
                 pkgs.add(Package(comps))
 
     return pkgs
@@ -53,7 +54,8 @@ def print_orphaned(repo):
     for f in glob.glob('*.pkg.tar.*'):
         comps = f.rsplit('-', 3)
         comps += comps.pop().split('.', 1)
-        pkg = Package(comps[:3])
+        comps[1] += '-' + comps[2]
+        pkg = Package(comps[:2])
         if pkg not in pkgs:
             print(f)
 
@@ -72,12 +74,47 @@ def list_installed():
 
     return pkgs
 
+
+def check_updates(repo, batch=False):
+    all_pkgs = sorted(list_packages(repo))
+
+    if batch:
+        pkg_map = {}
+        params = ['type=multiinfo']
+        for pkg in all_pkgs:
+            pkg_map[pkg.name] = pkg
+            params.append('arg[]={}'.format(pkg.name))
+
+        r = requests.get('https://aur.archlinux.org/rpc.php?{}'.format(
+            '&'.join(params)))
+        data = r.json()
+
+        for aurpkg in data['results']:
+            pkg = pkg_map[aurpkg['Name']]
+            if pyalpm.vercmp(aurpkg['Version'], pkg.ver) > 0:
+                yield pkg, aurpkg['Version']
+    else:
+        for pkg in all_pkgs:
+            r = requests.get(
+                'https://aur.archlinux.org/rpc.php?type=info&arg={}'.format(
+                    pkg.name))
+            data = r.json()
+            if type(data['results']) == dict:
+                aur_version = data['results']['Version']
+                if pyalpm.vercmp(aur_version, pkg.ver) > 0:
+                    yield pkg, aur_version
+            else:
+                print("warning: {} is not in the AUR".format(pkg.name),
+                      file=sys.stderr)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Arch repository helper")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--list-orphaned', '-o', action='store_true')
     group.add_argument('--list-packages', '-l', action='store_true')
     group.add_argument('--list-uninstalled', '-u', action='store_true')
+    group.add_argument('--check-updates', action='store_true')
     parser.add_argument('--pkgonly', action='store_true',
                         help="Do not include version in package lists")
     parser.add_argument('repo')
@@ -98,3 +135,7 @@ if __name__ == '__main__':
                 print(pkg.name)
             else:
                 print(pkg)
+    elif args.check_updates:
+        for pkg, ver in check_updates(args.repo):
+            # emulate the cower -b format to allow for parsing by other scripts
+            print(":: {pkg} -> {ver}".format(pkg=pkg, ver=ver))
