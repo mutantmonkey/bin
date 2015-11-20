fc3217c8ade640840efe93b26bda94a80e52a02f
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-11-19 23:06:01 -0800

    fix update_pkg_task.sh to search properly

--- update_pkg_task.sh
+++ update_pkg_task.sh
@@ -4,7 +4,7 @@ IFS=$'\n' pkgs=($(sed -r 's/^:: (\S+) .*$/\1/'))
 for pkg in $pkgs; do
     description="Update package: $pkg"
 
-    task "status=pending description=\"$description\"" minimal >/dev/null 2>&1
+    task +arch status:pending description:"$description" minimal >/dev/null 2>&1
     if [ $? -eq 1 ]; then
         task add +arch -- "$description"
     fi
