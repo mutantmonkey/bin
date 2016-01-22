f8d9f94b9f4a68c9721018d45b868d7dc0e18d7d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-01-21 22:40:21 -0800

    update noptime.sh

--- noptime.sh
+++ noptime.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-/usr/bin/python3 $HOME/code/mmhome/scheme.py -i 10.42.18.2 -d 100 all_off
+/usr/bin/python3 $HOME/code/mmhome/scheme.py -d 100 all_off
 /usr/bin/systemctl poweroff
