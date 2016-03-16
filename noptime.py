ad0c156c0c2833ffccdcf2f16fe3d08c9c14ee13
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-03-15 19:23:22 -0700

    replace noptime.sh with noptime.py

---
+++ noptime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import os.path
+import pyhugh
+import subprocess
+import yaml
+
+try:
+    import xdg.BaseDirectory
+    configpath = xdg.BaseDirectory.load_first_config('mmhome', 'config.yml')
+except:
+    configpath = os.path.expanduser('~/.config/mmhome/config.yml')
+
+config = yaml.safe_load(open(configpath))
+h = pyhugh.PyHugh(config['hue_bridge'], config['hue_username'])
+
+h.create_schedule({
+    'name': "Off",
+    'command': {
+        'address': "/api/{0}/groups/0/action".format(h.username),
+        'method': "PUT",
+        'body': {
+            'on': False,
+        },
+    },
+    'time': "PT00:00:15",
+    'autodelete': True,
+})
+
+subprocess.call(['/usr/bin/systemctl', 'poweroff'])
--- noptime.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-/usr/bin/python3 $HOME/code/mmhome/scheme.py -d 100 all_off
-/usr/bin/systemctl poweroff
