aa08696d00c8661b6a564591b360829e540716bf
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-03-26 19:51:01 -0700

    add rtpvol.sh

---
+++ rtpvol.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh -y
+
+inputs=("$(pactl list short sink-inputs | cut -f1,4 --output-delimiter=:)")
+
+for line in $inputs; do
+    s=("${(@s/:/)line}")
+    if [[ "${s[2]}" == "module-rtp-recv.c" ]]; then
+        input_index="${s[1]}"
+        break
+    fi
+done
+
+if [ -z "$input_index" ]; then
+    exit 1
+fi
+
+case "$1" in
+    mute)
+        pactl set-sink-input-mute "$input_index" toggle
+        ;;
+    up)
+        canberra-gtk-play -i audio-volume-change
+        pactl set-sink-input-volume "$input_index" +2%
+        ;;
+    down)
+        canberra-gtk-play -i audio-volume-change
+        pactl set-sink-input-volume "$input_index" -2%
+        ;;
+esac
